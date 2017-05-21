faa31a28faf36680ea0a96ddcaf383970bc77f1d
Maxime Rouyrre <rouyrre+git@gmail.com>
2017-05-21 15:02:02 +0200

    Update webpack prod config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require('path');
-const webpack = require('webpack')
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
+const path = require('path')
+const glob = require('glob')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const PurifyCSSPlugin = require('purifycss-webpack')
 
-const extractSass = new ExtractTextPlugin({
-    filename: "css/[name].css?[contenthash:8]",
-    disable: process.env.NODE_ENV === "development"
-});
+module.exports = function (env) {
+  const extractSass = new ExtractTextPlugin({
+    filename: 'css/[name].css?[contenthash:8]',
+    disable: env === 'dev'
+  })
+  const github = env === 'github'
 
-module.exports = function(env) {
-  const github = env && env.platform == 'github'
   return {
     entry: {
       bundle: './app/src/index.js',
@@ -20,7 +20,7 @@ module.exports = function(env) {
     output: {
       path: github ? path.resolve(__dirname, 'gh-pages') : path.resolve(__dirname, 'deploy/static'),
       publicPath: github ? '' : 'static/',
-      filename: 'js/[name].js?[chunkhash:8]',
+      filename: `js/[name].js${env === 'dev' ? '' : '?[chunkhash:8]'}`
     },
     module: {
       loaders: [
@@ -33,23 +33,23 @@ module.exports = function(env) {
         }, {
           test: /\.scss$/,
           use: extractSass.extract({
-            use: ['css-loader', 'sass-loader?sourceMap'],
+            use: ['css-loader?sourceMap', 'sass-loader?sourceMap'],
             fallback: 'style-loader',
             publicPath: '../'
           })
         }, {
           test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: "url-loader?limit=10000&mimetype=application/font-woff" +
-            "&hash=sha512&digest=hex&name=fonts/[name].[ext]?[hash:8]"
+          loader: 'url-loader?limit=10000&mimetype=application/font-woff' +
+            '&hash=sha512&digest=hex&name=fonts/[name].[ext]?[hash:8]'
         },
         {
           test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: "file-loader?hash=sha512&digest=hex&name=fonts/[name].[ext]?[hash:8]"
+          loader: 'file-loader?hash=sha512&digest=hex&name=fonts/[name].[ext]?[hash:8]'
         }, {
           test: /\.(jpe?g|png|gif|svg|ico)$/,
           loaders: [
-            'file-loader?hash=sha512&digest=hex&name=images/[name].[ext]?[hash:8]',
-          ],
+            'file-loader?hash=sha512&digest=hex&name=images/[name].[ext]?[hash:8]'
+          ]
         }
       ]
     },
@@ -58,8 +58,11 @@ module.exports = function(env) {
       new HtmlWebpackPlugin({
         template: 'app/index.html'
       }),
+      new PurifyCSSPlugin({
+        paths: glob.sync(path.join(__dirname, 'app/*.html'))
+      }),
       new CopyWebpackPlugin([{
-        from: 'app/favicon.ico',
+        from: 'app/favicon.ico'
       }, {
         from: 'app/robots.txt'
       }, {
@@ -67,5 +70,10 @@ module.exports = function(env) {
       }
       ])
     ],
+    devServer: {
+      host: '0.0.0.0',
+      disableHostCheck: true
+    },
+    devtool: env === 'dev' ? 'source-map' : false
   }
-};
+}
